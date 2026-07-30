c5d802a325aca6826de5a6d9c025e0b0e12c5574
Pedro Sgorla <pedrosgorladev@gmail.com>
2026-07-29 22:56:27 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		Department obj = new Department(1, "Books");
 		
@@ -43,6 +46,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update Seller");
+		
+		System.out.println("\n======= TESTE 6: Seller delete ========\n");
+		System.out.print("Enter id for delete: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete seller");
+		
+		sc.close();
 	}
 
 }
